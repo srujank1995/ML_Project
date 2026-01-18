018c076ac16f162f9fd6dad7f5000dcde66c609d
srujank1995 <srujankinjawadekar1@gmail.com>
2026-01-18 13:55:58 +0530

    data Ingestion created for train test & raw data

--- src/exception.py
+++ src/exception.py
@@ -1,12 +1,12 @@
 import sys
 
 def error_message_detail(error,error_detail:sys):
-    _,_,exec_tb = error_detail.exc_info()
-    file_name = exec_tb.tb_frame.f_code.co_filename
+    _,_,exc_tb = error_detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
     error_message ="Error occured in python script name [{0}] line number [{1}] and error message [{2}]".format(
-        file_name, exec_tb.tb_lineno, str(error)
+        file_name, exc_tb.tb_lineno, str(error)
     )
-
+    return error_message
 
 class CustomExecption(Exception):
     def __init__(self, error_message, error_detail:sys):
