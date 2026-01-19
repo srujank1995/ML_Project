675cad347a1113ccf071ff60f9f6e0775df4e1fd
srujank1995 <srujankinjawadekar1@gmail.com>
2026-01-19 21:26:46 +0530

    Data Transformation file added to gen pkl file

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,9 +1,10 @@
 import os 
 import sys 
-from src.exception import CustomExecption
+from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
-
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
@@ -42,8 +43,11 @@ class DataIngestion:
                 self.ingestion_config.test_data_path
             )
         except Exception as e:
-            raise CustomExecption(e,sys)
+            raise CustomException(e,sys)
 
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+    
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
